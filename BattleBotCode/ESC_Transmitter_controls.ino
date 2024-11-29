1dfc2e265a85be6d15e994ed98f096491019e71e
Diego Buenrostro <dbuenrostro64@gmail.com>
2024-11-29 15:49:22 -0800

    removed unneeded variable

--- BattleBotCode/ESC_Transmitter_controls.ino
+++ BattleBotCode/ESC_Transmitter_controls.ino
@@ -1,15 +1,13 @@
 #include "ESC.h"
 
-#define LED_PIN (13)  
 #define SIGNAL_PIN (8) 
 #define ESC_PIN (9)
+#define LED_PIN (13)  
 #define MAX_REVERSE (1000)
 #define MAX_FORWARD (2000)
 #define ARM_VAL (1500)                                        // Midpoint
 
 ESC myESC (ESC_PIN, MAX_REVERSE, MAX_FORWARD, ARM_VAL);       // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
-
-int speedSet;                                                 // Variable for the speed sent to the ESC
 
 int chValue;
 
