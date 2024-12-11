981aa689d05ad3df8a55d93de01b0474c11c9063
michelle-olson <60280318+michelle-olson@users.noreply.github.com>
2024-12-11 16:49:08 -0700

    Update ReceiverTankSteering.ino

--- BattleBotCode/ReceiverTankSteering.ino
+++ BattleBotCode/ReceiverTankSteering.ino
@@ -9,8 +9,8 @@
 #define MAX_FORWARD (2000)
 #define ARM_VAL (1500)                                        // Midpoint
 
-ESC myESCFont (ESC_PIN, MAX_REVERSE, MAX_FORWARD, ARM_VAL);       // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
-ESC myESC_fLess (ESC_PIN_fLess, MAX_REVERSE, MAX_FORWARD, ARM_VAL);  
+ESC myESCtext (ESC_PIN, MAX_REVERSE, MAX_FORWARD, ARM_VAL);       // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
+ESC myESC_tLess (ESC_PIN_fLess, MAX_REVERSE, MAX_FORWARD, ARM_VAL);  
 
 int chValue1;
 int chValue2;
@@ -18,14 +18,14 @@ int chValue2;
 
 void moveMotorsForward (int speed1, int speed2)
 {
-    myESCFont.speed(speed1); // set the first ESC speed to the input value (forwared direction)
+    myESCtext.speed(speed1); // set the first ESC speed to the input value (forwared direction)
     //1500-(speed2-1500)=3000-speed2
-    myESC_fLess.speed(3000-speed2); // set the second ESC speed to the reverse of input value
+    myESC_tLess.speed(speed2); // set the second ESC speed to the reverse of input value
 }
 void moveMotorsBackwards (int speed1, int speed2)
 {
-    myESCFont.speed(speed2); // set the first ESC speed to the input value (forwared direction)
-    myESC_fLess.speed(3000-speed1); // set the second ESC speed to the reverse of input value
+    myESCtext.speed(speed2); // set the first ESC speed to the input value (forwared direction)
+    myESC_tLess.speed(3000-speed1); // set the second ESC speed to the reverse of input value
 }
 
 
@@ -39,8 +39,8 @@ int readChannel(int channelInput, int minLimit, int maxLimit, int defaultValue)
 void setup() 
 {
     pinMode(LED_PIN, OUTPUT);                               // LED Visual Output
-    myESCFont.arm();                                            // Send the Arm value so the ESC will be ready to take commands
-    myESC_fLess.arm();
+    myESCtext.arm();                                            // Send the Arm value so the ESC will be ready to take commands
+    myESC_tLess.arm();
     digitalWrite(LED_PIN, HIGH);                            // LED High Once Armed
     delay(5000); 
     digitalWrite(LED_PIN, LOW);                             // Wait for a while
@@ -54,5 +54,31 @@ void loop()
     //myESCFont.speed(chValue1);   
     chValue2 = readChannel(SIGNAL_PIN2, MAX_REVERSE, MAX_FORWARD, ARM_VAL);
     //myESC_fLess.speed(chValue2);
-    moveMotorsForward(chValue1, chValue2);
+    //moveMotorsForward(chValue1, chValue2);
+    if (chValue1 >= 1600 ||  chValue1 <= 1400)
+    { //NEED TO DOUBLE CHECK IF PIVOT STEER IS ACTUALLY REVERSED. MAY ACTUALLY BE TRADITIONALLY STEERING
+      if (chValue1 <= 1400)
+      { //1500+(1500-chvalue1)=3000-chValue1; the value (1500-chValue1) is the distance from the midpoint value 1500
+        myESC_tLess.speed(3000-chValue1+200); //PIVOT STEER: we add 200 so it's offset from the other motor
+        myESCtext.speed(chValue1);
+        Serial.println(chValue1);
+      }
+      else
+      {
+        myESCtext.speed(chValue1);
+        Serial.println(chValue1);
+      }
+
+    }
+    else
+    {
+      myESCtext.speed(chValue2);
+      myESC_tLess.speed(chValue2); 
+      Serial.print("chValue1 ");
+      Serial.println(chValue1);
+    //  Serial.println("the else chValue2: "+ chValue2);
+      Serial.print("chValue2 ");
+      Serial.println(chValue2); 
+    }
+
 }
