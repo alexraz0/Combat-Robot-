80a8e8443586b37f3492c0a6951e941947f36317
michelle-olson <60280318+michelle-olson@users.noreply.github.com>
2025-04-09 19:24:41 -0700

    the code that worked on tues 4-9-25

--- BattleBotCode/steering-mrKrabs.ino
+++ BattleBotCode/steering-mrKrabs.ino
@@ -2,61 +2,51 @@
 
 //channel 1 is pin 8
 //channel 2 is pin 7
-//d10 is going M1
-//d9 is going M2
-
+//d9 is going M1
+//d10 is going M2
+//channel 2 goes left and right THIS IS FINAL
+//channel 1 goes up and down THIS IS FINAL 
 #define SIGNAL_PIN1 (8) 
 #define SIGNAL_PIN2 (7) 
-#define ESC_PIN_M2 (9)
-#define ESC_PIN_M1 (10)
+#define ESC_PIN_M1 (9)
+#define ESC_PIN_M2 (10)
 #define LED_PIN (13)  
 #define MAX_REVERSE (1125)
 #define MAX_FORWARD (1875)
-#define ARM_VAL (1500)
-/*
-6V/8V = 0.75
-Assumed 1500 = 0V and 2000 = 8V and 1000 = 8V
-2000 - 1500 = 500 which is the range the volts can span
-0.75(500) = 375 so we add this to the neutral value 1500 to get the 6 Volt max
-1500 + 375 = 1875 upper 6 volt max
-1500 - 375 = 1125  lower 6 volt max
-*/
+#define ARM_VAL (1500)                                        // Midpoint                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 
+ESC myESC_M2 (ESC_PIN_M2, MAX_REVERSE, MAX_FORWARD, ARM_VAL);       // ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
+ESC myESC_M1 (ESC_PIN_M1, MAX_REVERSE, MAX_FORWARD, ARM_VAL);  
 
-// ESC_Name (ESC PIN, Minimum Value, Maximum Value, Default Speed, Arm Value)
-ESC myESC_M1 (ESC_PIN_M1, MAX_REVERSE, MAX_FORWARD, ARM_VAL); 
-ESC myESC_M2 (ESC_PIN_M2, MAX_REVERSE, MAX_FORWARD, ARM_VAL); 
 
 int ch1Value;
 int ch2Value;
-int inverseMotorCH1;
-int inverseMotorCH2;
-bool exitRightLeft;
+
 
 
 
 
 // Testing Backwards
-void moveForwardBackward (int speed, int invSpeed)
-{   //channel 2, the right joystick moves up and down
+void moveForwardBackward (int speed)
+{   //channel 1, the right joystick moves up and down
         myESC_M1.speed(speed); 
-        myESC_M2.speed(invSpeed);
+        myESC_M2.speed(speed);                                                                                                                                                                                                                                                                            myESC_M2.speed(speed);
         
-        Serial.println("if ch2");
-        Serial.println(speed);
+       // Serial.println("if ch2");
+       // Serial.println(speed);
 }
 
 // works well as pure pivot turn
 void moveLeftRight (int speed, int invSpeed)
-{   //channel 2, the right joystick moves up and down
+{   //channel 2, the right joystick moves left and right
         myESC_M1.speed(speed); 
-        myESC_M2.speed(speed);
+        myESC_M2.speed(invSpeed);
 }
 
 
 
 
-//defaultValue is ARM_VAL=1500
+
 int readChannel(int channelInput, int minLimit, int maxLimit, int defaultValue)
 {
     int ch = pulseIn(channelInput, HIGH, 30000);
@@ -85,31 +75,19 @@ void loop()
     ch2Value = readChannel(SIGNAL_PIN2, MAX_REVERSE, MAX_FORWARD, ARM_VAL);
     //1500+(1500-speed)=3000-speed
     //1500-(speed-1500)=3000-speed
-    inverseMotorCH1 = 3000-ch1Value; 
-    inverseMotorCH2 = 3000-ch2Value; 
+    int inverseMotorCH1 = 3000-ch1Value; 
+    int inverseMotorCH2 = 3000-ch2Value; 
 
+     //moveForwardBackward(ch1Value); 
 
-    if (((ch1Value >= 1500) && (ch1Value <= ch2Value)) && (ch2Value>=1500))
-    { 
-       moveLeftRight(ch1Value, inverseMotorCH1);
+    if ((ch2Value >= 1605 ||  ch2Value <= 1300))
+    { //NEED TO DOUBLE CHECK IF PIVOT STEER IS ACTUALLY REVERSED. MAY ACTUALLY BE TRADITIONALLY STEERING
+       moveLeftRight(ch2Value, inverseMotorCH2);
     }
-    else if (((ch1Value >= 1500) && (ch1Value <= inverseMotorCH2)) && (ch2Value <= 1500))
-    { 
-       moveLeftRight(ch1Value, inverseMotorCH1);
-    }
-    else if ((ch1Value <= 1500) && (ch1Value >= ch2Value) && (ch2Value >= 1500))
+    else // (no ch1 input)
     {
-        moveLeftRight(ch1Value, inverseMotorCH1);
-    }
-    else if (((ch1Value <= 1500) && (ch1Value >= inverseMotorCH2)) && (ch2Value <= 1500))
-    {
-        moveLeftRight(ch1Value, inverseMotorCH1); 
-    }
-    else
-    {
-        moveForwardBackward(ch2Value, inverseMotorCH2);
+      moveForwardBackward(ch1Value); 
     }
 
-    delay(500);
 
 }
